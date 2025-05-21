4cb93288ec1af1bbed4bf1d73f02c1efed5f1b33
Martina <mnudel@itba.edu.ar>
2025-05-21 14:44:16 -0300

    Create keyboardDriver.h

---
+++ Kernel/include/keyboardDriver.h
@@ -0,0 +1,32 @@
+#ifndef KEYBOARD_DRIVER_H
+#define KEYBOARD_DRIVER_H
+
+#include <stdint.h>
+
+#define ESC 0x1B
+#define BACKSPACE 0x0E
+#define ENTER 0x1C
+#define SHIFT_L 0x2A
+#define SHIFT_R 0x36
+#define CTRL_L 0x1D
+#define CTRL_L_RELEASE 0x9D
+#define CAPSLOCK 0x3A
+#define RELEASE_OFFSET 0x80
+#define KEYS_AMOUNT 59
+
+/* Manejador de interrupciones para el teclado, 
+ * se ejecuta cuando el teclado genera una interrupcion
+*/
+void keyboard_interrupt_handler();
+
+/* 
+ * Devuelve un caracter desde el buffer del teclado o 0 si el buffer esta vacio
+*/
+char keyboard_read_getchar();
+
+/*
+ * Llena un arreglo r con los valores actuales de los registros del procesador
+*/
+uint64_t getRegisters(uint64_t * r);
+
+#endif // KEYBOARD_DRIVER_H
