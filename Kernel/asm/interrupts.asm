f3064c0d85940db19b93a9210050b13458eaa6c6
svegagarrot <svegagarrot@itba.edu.ar>
2025-05-22 20:02:29 -0300

    Cambio en handler para no perder valor de rax

--- Kernel/asm/interrupts.asm
+++ Kernel/asm/interrupts.asm
@@ -142,10 +142,39 @@ _irq05Handler:
 
 ;Syscall
 _irq80Handler:
-    pushState 
-    mov r9, rax
-    call syscallDispatcher
-    popState
+    push rbx
+    push rcx
+    push rdx
+    push rbp
+    push rdi
+    push rsi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+
+    mov   r9, rax
+    call  syscallDispatcher  ; devuelve en rax
+
+    pop   r15
+    pop   r14
+    pop   r13
+    pop   r12
+    pop   r11
+    pop   r10
+    pop   r9
+    pop   r8
+    pop   rsi
+    pop   rdi
+    pop   rbp
+    pop   rdx
+    pop   rcx
+    pop   rbx
+
     iretq
 
 ;Zero Division Exception
