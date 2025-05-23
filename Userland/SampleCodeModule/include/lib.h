ee25f32fd91d2ee0c9fb4249741e5bdb37b98c97
svegagarrot <svegagarrot@itba.edu.ar>
2025-05-23 13:29:04 -0300

    Faltaba la declaracion de fgets

--- Userland/SampleCodeModule/include/lib.h
+++ Userland/SampleCodeModule/include/lib.h
@@ -15,5 +15,6 @@ int strlen(const char *s);
 void strncpy(char *dest, const char *src, size_t n);
 int strcmp(const char *s1, const char *s2);
 int atoi(char* str);
+char *fgets(char *s, int n, int fd);
 
 #endif
