fbe92d70c1471b7e43e66cbde3bd73f2b3b2c133
svegagarrot <svegagarrot@itba.edu.ar>
2025-05-23 11:05:15 -0300

    uso las fucniones de lib.c

--- Userland/SampleCodeModule/shell/shell.c
+++ Userland/SampleCodeModule/shell/shell.c
@@ -1,7 +1,6 @@
 #include "../include/shell.h"
 #include "../include/commands.h"
 #include "../include/lib.h"
-#include <videoDriver.h> //despues hay que sacarlo
 
 static void shellPrompt();
 
@@ -11,39 +10,45 @@ void shellLoop(){
 
     char buffer[MAX_LINE_LENGTH];
 
-    video_putString(WELCOME_MESSAGE, 0xFFFFFF, 0x000000); 
+    printf("%s", WELCOME_MESSAGE);
 
     while(1){
         shellPrompt();
-        //scanf(buffer, MAX_LINE_LENGTH);
 
-        if(strcmp(buffer, "exit") == 0){
-            break;
+        if(fgets(buffer, MAX_LINE_LENGTH, stdin) == NULL){
+            printf("%s", "Error al leer la linea\n");
+            continue;
         }
+
+        size_t len = strlen(buffer);
+        if (len > 0 && buffer[len-1] == '\n') {
+            buffer[len-1] = '\0';
+        }
+
+        if (strcmp(buffer, "exit") == 0) {
+            return;
+        }
+
 
         int found = 0;
 
         for(int i = 0; shellCmds[i].name; i++){
             if(strcmp(buffer, shellCmds[i].name) == 0){
-
-                if(strcmp(buffer, "exit") == 0){
-                    return;
-                }
-
                 shellCmds[i].function();
                 found = 1;
                 break;
             }
         }
 
-        if(!found){{
-            video_putString(NOT_FOUND_MESSAGE, FOREGROUND_COLOR, BACKGROUND_COLOR);
-        }}
+        if(!found){
+            printf("%s", NOT_FOUND_MESSAGE);
+        }
     }
 }
 
 static void shellPrompt(){
-    video_putString("TP-ARQUI-", FOREGROUND_COLOR, BACKGROUND_COLOR);
-    video_putString(shellUser, FOREGROUND_COLOR, BACKGROUND_COLOR);
-    video_putString(":~$ ", FOREGROUND_COLOR, BACKGROUND_COLOR);
+
+    printf("%s", "TP-ARQUI-");
+    printf("%s", shellUser);
+    printf("%s", ":~$ ");
 }
